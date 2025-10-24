77b7e79e1d53f9197e4ea99dc4098e85cde697d6
yusupscopes <yusupmaulana950@gmail.com>
2025-10-24 13:05:37 +0700

    feat: authenticaion ui

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">{children}</div>
+    </div>
+  );
+}
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,5 @@
+import SignUpView from "@/modules/auth/ui/views/sign-up-view";
+
+export default function SignUp() {
+  return <SignUpView />;
+}

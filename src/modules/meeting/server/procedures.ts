8512880625905e7bf4e3a92691e4637296c29e49
yusupscopes <yusupmaulana950@gmail.com>
2025-11-24 09:02:39 +0700

    feat: meeting setup

---
+++ src/modules/meeting/server/procedures.ts
@@ -0,0 +1,89 @@
+import { database } from "@/database";
+import { meeting } from "@/database/schema";
+import {
+  baseProcedure,
+  createTRPCRouter,
+  protectedProcedure,
+} from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
+import {
+  DEFAULT_PAGE,
+  DEFAULT_PAGE_SIZE,
+  MAX_PAGE_SIZE,
+  MIN_PAGE_SIZE,
+} from "@/constants";
+
+export const meetingRouter = createTRPCRouter({
+  getMany: protectedProcedure
+    .input(
+      z.object({
+        page: z.number().default(DEFAULT_PAGE),
+        pageSize: z
+          .number()
+          .min(MIN_PAGE_SIZE)
+          .max(MAX_PAGE_SIZE)
+          .default(DEFAULT_PAGE_SIZE),
+        search: z.string().nullish(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { page, pageSize, search } = input;
+
+      const items = await database
+        .select({
+          ...getTableColumns(meeting),
+        })
+        .from(meeting)
+        .where(
+          and(
+            eq(meeting.userId, ctx.auth.user.id),
+            search ? ilike(meeting.name, `%${search}%`) : undefined
+          )
+        )
+        .orderBy(desc(meeting.createdAt), desc(meeting.id))
+        .limit(pageSize)
+        .offset((page - 1) * pageSize);
+
+      const [total] = await database
+        .select({ count: count() })
+        .from(meeting)
+        .where(
+          and(
+            eq(meeting.userId, ctx.auth.user.id),
+            search ? ilike(meeting.name, `%${search}%`) : undefined
+          )
+        );
+
+      const totalPages = Math.ceil(total.count / pageSize);
+
+      return {
+        items,
+        totalItems: total.count,
+        totalPages,
+      };
+    }),
+
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const [existingMeeting] = await database
+        .select({
+          ...getTableColumns(meeting),
+        })
+        .from(meeting)
+        .where(
+          and(eq(meeting.id, input.id), eq(meeting.userId, ctx.auth.user.id))
+        );
+
+      if (!existingMeeting) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: `Meeting with id ${input.id} not found`,
+        });
+      }
+
+      return existingMeeting;
+    }),
+});
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { agentRouter } from "@/modules/agent/server/procedures";
 import { createTRPCRouter } from "../init";
+import { meetingRouter } from "@/modules/meeting/server/procedures";
 
 export const appRouter = createTRPCRouter({
   agent: agentRouter,
+  meeting: meetingRouter,
 });
 
 // export type definition of API
